9e7d292863a89861419285136759318dee39edf7
LinNanQuan <49220824@qq.com>
2017-01-15 16:22:27 +0800

    修复html页面的显示

--- src/main/java/com/southernbox/inf/util/ServerAPI.java
+++ src/main/java/com/southernbox/inf/util/ServerAPI.java
@@ -3,6 +3,7 @@ package com.southernbox.inf.util;
 public class ServerAPI {
     //public static final String BASE_URL = "http://192.168.1.112:8080/Ice&Fire";
 //    public static final String BASE_URL = "http://buypaint.aliapp.com";
+    public static final String GITHUB_HTML_PREVIEW = "http://htmlpreview.github.io/?";
     public static final String BASE_URL = "https://raw.githubusercontent.com/SouthernBox/IceAndFireServer/master";
-	public static final String OPTION_URL = BASE_URL + "/option.json";
+    public static final String OPTION_URL = BASE_URL + "/option.json";
 }
