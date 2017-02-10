d3791678fa91e98d3344b0fbcf22eca381ba4c75
LinNanQuan <linnq@zdnst.com>
2017-02-10 11:55:26 +0800

    添加GreenDao依赖

--- app/src/main/java/com/southernbox/inf/util/RequestServes.java
+++ app/src/main/java/com/southernbox/inf/util/RequestServes.java
@@ -3,7 +3,6 @@ package com.southernbox.inf.util;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 /**
  * Created by nanquan.lin on 2017/1/19 0019.
