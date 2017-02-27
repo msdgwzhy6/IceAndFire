7c86920d695f40e293246fb3023a319f30848cbc
LinNanQuan <49220824@qq.com>
2017-02-27 22:51:42 +0800

    优化本地js代码

--- app/src/main/assets/skip.js
+++ app/src/main/assets/skip.js
@@ -1,3 +1,5 @@
 ﻿function CatelynTully(){
-		javascript:Android.goDetail('凯特琳·徒利','/person/Tully/Catelyn_Tully/Catelyn_Tully.png','/person/Tully/Catelyn_Tully/Catelyn_Tully.html');
+		javascript:Android.goDetail('凯特琳·徒利',
+		'/person/Tully/Catelyn_Tully/Catelyn_Tully.png',
+		'/person/Tully/Catelyn_Tully/Catelyn_Tully.html');
 }
