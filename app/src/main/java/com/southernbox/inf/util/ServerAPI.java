e0c5b6e261ee834543a71b525009cb47e2aaef3b
SouthernBox <49220824@qq.com>
2017-04-30 22:51:19 +0800

    使用九牛云存储数据

--- app/src/main/java/com/southernbox/inf/util/ServerAPI.java
+++ app/src/main/java/com/southernbox/inf/util/ServerAPI.java
@@ -7,6 +7,9 @@ package com.southernbox.inf.util;
 
 public class ServerAPI {
 
-    public static final String BASE_URL = "https://southernbox.github.io/IceAndFireServer/";
+    //GitHub
+    //public static final String BASE_URL = "https://southernbox.github.io/IceAndFireServer/";
+    //九牛云
+    public static final String BASE_URL = "http://op86bjpor.bkt.clouddn.com/";
 
 }
